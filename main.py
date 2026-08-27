650bb6fae0592c7bc90a3126bb6d4c6845e6db4b
hilmyha <hilmyahmadhaidar@gmail.com>
2026-08-27 16:52:20 +0700

    feat: CD feature self hosted

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/haidar")
+def haidar():
+    return {"Hello": "Haidar"}
